619447a074b4f437f9ff6d5d5a3a364c588379d9
Hugo Lundin <hugo@lundin.dev>
2023-09-17 14:34:38 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for fileicon.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ... 
+  }: flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        packageName = "fileicon";
+      in {
+        packages.fileicon = pkgs.callPackage ./${packageName}.nix {};
+        defaultPackage = self.packages.${system}.${packageName};
+      });
+}
